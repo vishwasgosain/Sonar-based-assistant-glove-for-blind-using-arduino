7f57b4e4e2fc74142a4a672f94d1e71495a7a0bc
Vishwas Gosain <gosainvishwas@gmail.com>
2016-10-14 18:25:47 +0530

    Arduino code that includes interfacing with speaker, sonar and vibration output.

---
+++ igdtu_glove.ino
@@ -0,0 +1,29 @@
+int trigpin=11;
+int echopin=12;
+long duration,cm,inches;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(trigpin,OUTPUT);
+pinMode(echopin,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(trigpin, LOW);
+delayMicroseconds(5);
+digitalWrite(trigpin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigpin, LOW);
+pinMode(echopin,INPUT);
+duration = pulseIn(echopin,HIGH);
+cm = (duration/2)/29.1;
+inches = (duration/2)/74;
+
+Serial.print(inches);
+Serial.print("inches           ");
+Serial.print(cm);
+Serial.print("centimeters            ");
+
+delay(2500); 
+}
